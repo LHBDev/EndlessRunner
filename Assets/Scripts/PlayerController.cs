ba1776a2e63d79e4a8596e818e59e8dd3c628e34
LHBDev <freeraprb@gmail.com>
2017-07-22 13:45:06 -0500

    Adding Scene and fixing jump mechanic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,26 +17,31 @@ public class PlayerController : MonoBehaviour {
     const int STATE_HURT = 3;
     int _current_state = STATE_RUN;
 
+    bool _isHurt = false;
+    bool _isJumping = false;
+
     int hp = 100;
     int lives = 3;
 
 	// Use this for initialization
 	void Start () {
         animator = this.GetComponent<Animator>();
+        myRigidbody = this.GetComponent<Rigidbody2D>();
 	}
 	
     void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !_isHurt && !_isJumping)
+        {
+            _isJumping = true;
             changeState(STATE_JUMP);
+            myRigidbody.AddForce(new Vector2(0, 250));
+            
+        }
         else if (Input.GetKeyDown(KeyCode.LeftShift))
             changeState(STATE_SHOOT);
-        else if (Input.GetKeyUp(KeyCode.Space))
-            changeState(STATE_RUN);
         else if (Input.GetKeyUp(KeyCode.LeftShift))
             changeState(STATE_RUN);
-       
-            
     }
 
     void changeState(int state)
@@ -59,5 +64,14 @@ public class PlayerController : MonoBehaviour {
                 break;
         }
         _current_state = state;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.gameObject.name == "Floor")
+        {
+            _isJumping = false;
+            changeState(STATE_RUN);
+        }
     }
 }
