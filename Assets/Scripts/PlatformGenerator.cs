aa815ce3ef2f5d08f37420bf226a2bb82beb1b97
Daniel Delgado <vennnot@gmail.com>
2017-07-22 19:06:49 -0500

    Increased Difficulty

--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class PlatformGenerator : MonoBehaviour {
 
-    public GameObject thePlatform;
+    public GameObject[] thePlatform;
 
     public float timer = 0f;
+
+    private int number;
+
     // Use this for initialization
     void Start () {
+        number = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         
         if(timer>1f)
-        { 
-
-            Instantiate(thePlatform, transform.position, transform.rotation);
-
+        {
+            
+            Instantiate(thePlatform[number], transform.position, transform.rotation);
+            number = Mathf.FloorToInt(Random.Range(0f, 2.9f));
             timer = 0f;
         }
         else
--- Assets/Scripts/PlatformSpeed.cs
+++ Assets/Scripts/PlatformSpeed.cs
@@ -6,6 +6,8 @@ public class PlatformSpeed : MonoBehaviour {
 
     private Rigidbody2D rigidBody;
 
+    private float timer;
+
     // Use this for initialization
     void Start () {
         rigidBody = GetComponent<Rigidbody2D>();
@@ -14,5 +16,10 @@ public class PlatformSpeed : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         rigidBody.velocity = new Vector2(-5f,0);
+        timer+= Time.deltaTime;
+        if(timer > 3.5)
+        {
+            Destroy(gameObject);
+        }
     }
 }
